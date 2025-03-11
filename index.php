be0d9eccdd224f6571790401be973343de79926a
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-03-10 21:44:01 -0600

    Cambios en los enlaces en index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Registro</title>
-    <link rel="icon" type="image/png" href="/assets/img/UNAH-escudo.png">
-    <link rel="stylesheet" href="/css/indexStyles.css">
+    <link rel="icon" type="image/png" href="assets/img/UNAH-escudo.png">
+    <link rel="stylesheet" href="views/css/indexStyles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/6130fb0810.js" crossorigin="anonymous"></script>
 </head>
@@ -13,29 +13,29 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <img src="/assets/img/logo-unah.png" alt="">
+                <img src="views/assets/img/logo-unah.png" alt="">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="/js/modules/construccion.php">Estudiantes</a>
+                            <a class="nav-link" aria-current="page" href="views/construccion.php">Estudiantes</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/js/modules/construccion.php">Docentes</a>
+                            <a class="nav-link" href="views/construccion.php">Docentes</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#reviewerModal">Revisores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/js/modules/adminLogin.php">Administradores</a>
+                            <a class="nav-link" href="views/adminLogin.php">Administradores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/js/modules/admisiones.php">Admisiones</a>
+                            <a class="nav-link" href="views/admisiones.php">Admisiones</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/js/modules/construccion.php">Biblioteca</a>
+                            <a class="nav-link" href="views/construccion.php">Biblioteca</a>
                         </li>
                     </ul>
                 </div>
@@ -54,26 +54,26 @@
                   </nav>
             </div>
             <div class="banner-container">
-                <img src="/assets/img/1.jpg" alt="Descripción" class="img-fluid">
+                <img src="views/assets/img/1.jpg" alt="Descripción" class="img-fluid">
             </div>
         </div>
         <section class="box">
             <nav class="user-selection">
                 <div class="row w-100">
                     <div class="col">
-                        <a href="/js/modules/construccion.php">
+                        <a href="views/construccion.php">
                             <div><i class="fa-solid fa-user"></i></div>
                             <strong>Estudiantes</strong><br>Ingresa como estudiante
                         </a>
                     </div>
                     <div class="col">
-                        <a href="/js/modules/construccion.php">
+                        <a href="views/construccion.php">
                             <div><i class="fa-solid fa-user-tie"></i></div>
                             <strong>Docentes</strong><br>Ingresa como docente
                         </a>
                     </div>
                     <div class="col">
-                        <a href="/adminLogin.php">
+                        <a href="views/adminLogin.php">
                             <div><i class="fa-solid fa-user-plus"></i></div>
                             <strong>Administradores</strong><br>Ingresa como administrador
                         </a>
@@ -82,7 +82,7 @@
             </nav>
             <div class="aside-container">
                 <aside class="image-container">
-                    <img src="/assets/img/2.jpg" alt="Imagen ilustrativa">
+                    <img src="views/assets/img/2.jpg" alt="Imagen ilustrativa">
                 </aside>
                 <aside class="content">
                     <div class="line">
@@ -115,8 +115,8 @@
                 </div>
                 <div class="modal-body d-flex justify-content-center align-items-center">
                     <div class="btn-group" role="group" aria-label="Select reviewer type">
-                        <a href="/js/modules/loginSolicitudesAdmision.php"><button type="button" class="btn btn-warning mx-3" id="admissionReviewer">Revisor de Solicitud de Admision</button></a>
-                        <a href="/js/modules/loginRevisoreExamenes.php"><button type="button" class="btn btn-warning mx-3" id="examReviewer">Revisor de Examen de Admision</button></a>  
+                        <a href="views/loginSolicitudesAdmision.php"><button type="button" class="btn btn-warning mx-3" id="admissionReviewer">Revisor de Solicitud de Admision</button></a>
+                        <a href="views/loginRevisoreExamenes.php"><button type="button" class="btn btn-warning mx-3" id="examReviewer">Revisor de Examen de Admision</button></a>  
                     </div>
                 </div>
             </div>
