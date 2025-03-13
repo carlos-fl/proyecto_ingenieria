cf18316d2fc74b2cf6dde5994b05f3b6f583319d
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-03-13 15:10:44 -0600

    Creación de Web Components de Login y Pop Up

---
+++ public/views/js/components/login-form.js
@@ -0,0 +1,158 @@
+class LoginForm extends HTMLElement{
+
+    constructor(){
+        super()
+        this.action = ""
+        this.imgSource = ""
+        this.imgAlt = ""
+        this.heading =""
+        this.message = ""
+    }
+
+    handleEvent(event){
+        this.login(event)
+    }
+
+    connectedCallback(){
+        this.action = this.getAttribute("action")
+        this.imgSource = this.getAttribute("imgSource")
+        this.imgAlt = this.getAttribute("imgAlt") ?? ""
+        this.heading = this.getAttribute("heading")
+        this.message = this.getAttribute("message") ?? "Debes autenticarte para usar este servicio"
+        this.render()
+        this.querySelector("#submitBtn").addEventListener("click", this)
+    }
+
+    render(){
+        this.innerHTML = `
+            <section id="loginSection" class="container d-flex justify-content-center">
+                <div id="loginComponent" class="outline-grey">
+                <img id="heroUnah" src="${this.imgSource}" alt="${this.imgAlt}" >
+                <div id="loginBox" class="pt-3 ps-3 pe-5">
+                    <h3 class="fw-bold">${this.heading}</h3>
+                    <small>${this.message}</small>
+                    <p class="mt-5">Ingrese su correo y contraseña</p>
+                    <hr>
+                    <div id="loginForm" class="mt-5">
+                        <div>
+                            <label for="email">Correo</label>
+                            <input id="email" placeholder="example@unah.edu.hn" type="email" class="form-control">
+                        </div>
+                        <div class="mt-2">
+                            <label for="password">Contraseña</label>
+                            <input id="password" placeholder="Contraseña" type="password" class="form-control">
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button id="submitBtn" class="btn btn-primary mt-3">Ingresar</button>
+                        </div>
+                        <small class="float-end mt-2"><a href="" class="text-danger no-underline">Olvidé mi contraseña</a></small>
+                    </div>
+                </div>
+            </div>
+        </section>
+    `
+    }
+    
+
+    login(event){
+        //Bloquear botón
+        let submitBtn = event.target
+        let emailInput = this.querySelector("#email")
+        let passwordInput = this.querySelector("#password")
+        let email = emailInput.value.trim()
+        let password = passwordInput.value.trim()
+        this.disable(submitBtn, 2000)
+        if (!email || !password){
+            // Pedirle al usuario que ponga un usuario y una contraseña
+            this.changeBorder(emailInput,"var(--bs-border-width)", "red")
+            this.changeBorder(passwordInput,"var(--bs-border-width)", "red")
+            this.emit("fail", {message: "Ingrese su usuario y contraseña"});
+            return
+        }
+        if (!this.validEmail(email)){
+            // Pedirle al usuario que ingrese un correo válido
+            this.changeBorder(emailInput,"var(--bs-border-width)", "red")
+            this.changeBorder(passwordInput,"var(--bs-border-width)", "var(--bs-border-color)")
+            this.emit("fail", {message: "Ingrese un correo válido"});
+            return
+        }
+        // Realizar petición
+        this.requestLogin(email, password)
+    }
+
+    disable(htmlElement, time){
+        htmlElement.setAttribute("disabled", "disabled")
+        setTimeout(() => {htmlElement.removeAttribute("disabled")}, time)
+    }
+
+    changeBorder(input, borderWidth, color){
+        // Add red border to an input in the form
+        input.style.border = borderWidth + ' solid ' + color 
+
+    }
+
+    emit(eventName, detail={}){
+        // Emit an event
+        let event = new CustomEvent(`login-form:${eventName}`, {
+            bubbbles: true,
+            cancelable: true,
+            detail: detail
+            })
+        
+        this.dispatchEvent(event)
+    }
+
+    validEmail(email){
+        let pattern = /[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        let re = new RegExp(pattern)
+        return re.test(email)
+    }
+
+    async requestLogin(email, password){
+        let body = JSON.stringify({
+            "email": email,
+            "password": password
+        })
+        fetch(this.action, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: body
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (!data){
+                this.emit("fail", {message: "Hubo un error con el servidor"});
+                return
+            }
+            if (data.status == "failure"){
+                // No se pudo loggear correctamente
+                this.changeBorder(emailInput,"var(--bs-border-width)", "red")
+                this.changeBorder(passwordInput,"var(--bs-border-width)", "red")
+                this.emit("fail", {message: "Usuario o contraseña incorrectos"});
+                return  
+            }
+            // Successful login
+            // Load user data to sessionStorage
+            this.#loadUserData(data.user)
+            // TODO: Considerar ramificaciones. Quizas disparando un evento?
+            window.location.href ="adminProfile.php"
+        })
+        .catch(error => {
+            // Unsuccessful login
+            this.emit("fail", {message: "Hubo un error con el servidor"});
+        })
+    }
+
+    #loadUserData(user){
+        window.sessionStorage.setItem("userId", user.USER_ID)
+        window.sessionStorage.setItem("userFirstName", user.FIRST_NAME) 
+        window.sessionStorage.setItem("userLastName", user.LAST_NAME)
+        window.sessionStorage.setItem("userPhoneNumber", user.PHONE_NUMBER) 
+        window.sessionStorage.setItem("userInstEmail", user.INST_EMAIL) 
+    }
+}
+
+
+customElements.define("login-form", LoginForm)
---
+++ public/views/js/components/pop-up.js
@@ -0,0 +1,53 @@
+class PopUp extends HTMLElement{
+
+    constructor(){
+        super()
+        this.message = ""
+        this.imgSource = ""
+        this.duration = ""
+    }
+
+    static observedAttributes = ["message", "imgSource", "popUpClass"]
+
+    connectedCallback(){
+        this.message = this.getAttribute("message")
+        this.imgSource = this.getAttribute("imgSource")
+        this.popUpClass = this.getAttribute("popUpClass") ?? "fail-popup"
+        // Default pop Up duration time is 4s
+        this.duration = this.getAttribute("duration") ?? 4000
+        this.render()
+    }
+
+    attributeChangedCallback(){
+        this.connectedCallback()
+    }
+
+    render(){
+        this.innerHTML = `
+            <div id="popUpContainer" class="d-flex align-items-center ps-3 pop-up ${this.popUpClass}" disabled>
+                <img id="popUpImg"src="${this.imgSource}" alt="">
+                <span class="ms-2" id="popUpMessage">${this.message}</span>
+            </div>
+    `
+    }
+
+    show(){
+        // Show the popUp
+        let keyframes = [
+            {display: "block", bottom: "20px", opacity: 1, offset: .01},
+            {display: "block", bottom: "20px", opacity: 1, offset: .9},
+            {display: "block", bottom: "0px", opacity: 0, offset: 1},
+        ]
+        let timing = {
+            duration: this.duration, 
+            easing: "ease-in",
+            iterations: 1
+        }
+        let container = this.querySelector("#popUpContainer")
+        let animation = container.animate(keyframes, timing)
+        animation.play()
+    }
+}
+
+
+customElements.define("pop-up", PopUp)
