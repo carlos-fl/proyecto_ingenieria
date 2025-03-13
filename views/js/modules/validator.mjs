81d667e5d262012677bfade82706ca5d6bef7f7f
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-03-13 15:16:02 -0600

    Deleted extra views folder on project root

--- views/js/modules/validator.mjs
+++
@@ -1,7 +0,0 @@
-// Module to validate form input
-
-export function validEmail(email){
-    let pattern = /[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    let re = new RegExp(pattern)
-    return re.test(email)
-}
