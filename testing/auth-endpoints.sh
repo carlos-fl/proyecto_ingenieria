ec72b53b02954c99c42d7127459b137c06ddf62b
carlos <carlosftru@gmail.com>
2025-03-16 19:56:48 -0600

    made logout and fixing auth

--- testing/auth-endpoints.sh
+++ testing/auth-endpoints.sh
@@ -12,11 +12,21 @@ auth_login () {
   curl -s -X POST -d $body_jo $endpoint | jq
 }
 
+logout () {
+  endpoint=$1
+  desc=$2
+  echo $endpoint: $desc
+  curl -s -X POST -d $body_jo $endpoint | jq
+}
+
 auth_login $(jo email=carlos.rodriguez@unah.hn password=1234) "$URL/auth/controllers/adminLogin.php" "auth de admin"
 auth_login $(jo email=mfernandez@unah.hn password=1234) "$URL/auth/controllers/teacherAuth.php" "auth de docentes"
 auth_login $(jo email=carlos@gmail.com password=1234) "$URL/auth/controllers/applicantReviewer.php" "auth de revisor de datos de aplicante"
 auth_login $(jo email=carlos@gmail.com password=1234) "$URL/auth/controllers/examReviewer.php" "auth de revisor de examenes"
 
+# log out
+logout "$URL/auth/controllers/logout.php" "Endpoint to logout"
+
 # test applicant
 echo "auth de aplicante cuando ingrese a revisar datos enviados"
-curl -s -X POST -d $(jo applicantCode=12345 email=applicant@gmail.com) "$URL/auth/controllers/applicantAuth.php" | jq
+curl -s -X POST -d $(jo applicantCode=20221000001 email=carlos.mejia@unah.edu.hn) "$URL/auth/controllers/applicantAuth.php" | jq
