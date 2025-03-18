d892fc0557e9467017e84f281af9338b9ca5986e
Jerh21 <hamjosue27@gmail.com>
2025-03-17 18:24:13 -0600

    adminsEmployees funcional y correccion createAdmis

--- public/api/admissions/controllers/createAdmission.php
+++ public/api/admissions/controllers/createAdmission.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . "/../../../../../config/database/Database.php";
-require_once __DIR__ . "/../../../../../config/env/Environment.php";
+require_once __DIR__ . "/../../../../config/database/Database.php";
+require_once __DIR__ . "/../../../../config/env/Environment.php";
 
 header("Content-Type: application/json");
 
