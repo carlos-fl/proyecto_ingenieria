3d95e064c0b43efbb8828816e13893cc0db1d4f7
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-03-17 21:49:52 -0600

    Lógica asíncrona para inscribirse a la UNAH

---
+++ public/views/js/inscripciones.js
@@ -0,0 +1,161 @@
+// Lógica para la carga de la vista inscripciones.php
+import { changeBorder, showPopUp } from "./modules/utlis.mjs"
+import { validDNI, validEmail, validPhoneNumber } from "./modules/validator.mjs"
+
+function fetchMajorsByRegionalCenter(event){
+    let selectedRegionalCenter = event.target
+    let centerId = selectedRegionalCenter.value
+    let urlParams = new URLSearchParams({"center": centerId})
+    let mainMajorSelect = document.getElementById("main-career")
+    let secondaryMajorSelect = document.getElementById("secondary-career")
+    fetch(`/api/resources/controllers/majorsByCenter.php?${urlParams}`, {METHOD:"GET"})
+    .then(response =>{ return response.json()})
+    .then(data => {
+        mainMajorSelect.innerHTML = ""
+        secondaryMajorSelect.innerHTML = ""
+        for (let major of data["data"]){
+            let option = document.createElement("option")
+            let majorId = major["MAJOR_ID"]
+            let majorName = major["MAJOR_NAME"]
+            option.innerText = majorName
+            option.value = majorId
+            mainMajorSelect.appendChild(option)
+            let clone = option.cloneNode(true)
+            secondaryMajorSelect.appendChild(clone)
+        }
+
+    })
+    // TODO: Implement pop up to show error message
+    .catch(console.log("No se puedieron obtener los datos de este centro"))
+    
+    
+}
+
+function fetchRegionalCenters(){
+    // Fetch los centros regionales de la universidad
+    let regionalCentersSelect = document.getElementById("regional-center")
+    fetch("/api/resources/controllers/regionalCenters.php", {METHOD:"GET"})
+    .then(response => response.json())
+    .then(data => {
+        for (let center of data["data"]){
+            let option = document.createElement("option")
+            option.innerText = center["CENTER_NAME"]
+            option.value = center["CENTER_ID"]
+            option.onclick = fetchMajorsByRegionalCenter
+            regionalCentersSelect.appendChild(option)
+        }
+    })
+    // TODO: Implementar una vista por errores con el servidor
+    .catch(error => {
+        console.log("No se pudo conectar con el servidor, intente mas tarde")
+        console.log(error)
+    })
+}
+
+function cleanFormStyle(form){
+    // Devuelve el formulario a estilo original
+    for (let control of form){
+        changeBorder(control, "var(--bs-border-width)", "var(--bs-border-color)")
+    }
+}
+
+function emptyFormControls(form){
+    // Retorna una lista con los controls que estén vacíos
+    let empty = []
+    for (let control of form ){
+        if (control.value === "" || control.value === null) empty.push(control)
+    }
+    return empty
+}
+
+function emptyControlStyle(emptyControls){
+    // Darles en border rojo a los controles dados
+    for (let control of emptyControls){
+        changeBorder(control, "var(--bs-border-width)", "red")
+    }
+}
+
+function differentOptions(mainCareer, secondaryCareer){
+    // Verificar si se eligieron dos carreras diferentes
+    return mainCareer.value !== secondaryCareer.value
+}
+
+function submitForm(event){
+    let firstName = document.getElementById("first-name")
+    let lastName = document.getElementById("last-name")
+    let dni = document.getElementById("user-id")
+    let phone = document.getElementById("phone")
+    let email = document.getElementById("email")
+    let genderSelect = document.getElementById("gender")
+    let regionalCentersSelect = document.getElementById("regional-center")
+    let mainCareerSelect = document.getElementById("main-career")
+    let secondaryCareerSelect = document.getElementById("secondary-career")
+    let certificateFile = document.getElementById("file-upload")
+    let form = [firstName, lastName, dni, phone, email, genderSelect, regionalCentersSelect, mainCareerSelect,          secondaryCareerSelect, certificateFile]
+    let emptyControls = emptyFormControls(form)
+    cleanFormStyle(form)
+    if (emptyControls.length != 0){
+        // Indicar que se debe de llenar los campos obligatorios
+        emptyControlStyle(emptyControls)
+        showPopUp("Porfavor, Llene todos los datos")
+        return
+    }
+    if(!validDNI(dni.value)){
+        emptyControlStyle([dni])
+        showPopUp("Ingrese un DNI válido")
+        return 
+    }
+    if (!validPhoneNumber(phone.value)){
+        emptyControlStyle([phone])
+        showPopUp("Ingrese un número válido")
+        return
+    }
+    if (!validEmail(email.value)){
+        emptyControlStyle([email])
+        showPopUp("Ingrese un correo válido")
+        return
+    }
+    if (!differentOptions(mainCareerSelect, secondaryCareerSelect)){
+        console.log("MISMA CARRERA!")
+        emptyControlStyle([mainCareerSelect, secondaryCareerSelect])
+        showPopUp("Elija dos carreras distintas")
+        return
+    }
+    let body = JSON.stringify({
+        "firstName": firstName.value.trim(),
+        "lastName": lastName.value.trim(),
+        "dni": dni.value.trim(),
+        "phoneNumber": phone.value.trim(),
+        "email": email.value.trim(),
+        "gender": genderSelect.value,
+        "primaryMajor": mainCareerSelect.value,
+        "secondaryMajor": secondaryCareerSelect.value,
+        "comment": "",
+        "certificate": certificateFile[0]
+
+    })
+    fetch("/api/admissions/controllers/createAdmission.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: body
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Se guardó la solicitud exitosamente!")
+    })
+    .catch(console.log("No se pudo guardar la solicitud"))
+
+
+}
+
+
+function main(){
+    fetchRegionalCenters()
+    let submitBtn = document.getElementById("submit-button")
+    submitBtn.addEventListener("click", submitForm)
+}
+
+
+main()
