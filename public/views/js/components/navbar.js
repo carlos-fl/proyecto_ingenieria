48171692de38389c390303598adf88a9dfde7548
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-03-17 21:57:17 -0600

    Patch: Corrected relative paths in navbar

--- public/views/js/components/navbar.js
+++ public/views/js/components/navbar.js
@@ -25,18 +25,18 @@ class Navbar extends HTMLElement{
         // valores por defecto.
        // Se definen los valores por defecto si no se especifican los atributos.
     const homeHref = this.getAttribute('home-href') || '/index.php';
-    const logo = this.getAttribute('logo') || 'assets/img/logo-unah.png';
+    const logo = this.getAttribute('logo') || '/views/assets/img/logo-unah.png';
     const logoAlt = this.getAttribute('logo-alt') || 'Logo UNAH';
-    const estudiantesHref = this.getAttribute('estudiantes-href') || './loginEstudiantes.php';
-    const docentesHref = this.getAttribute('docentes-href') || './loginDocentes.php';
-    const matriculaHref = this.getAttribute('matricula-href') || './login.php';
-    const admisionesHref = this.getAttribute('admisiones-href') || './admisiones.php';
+    const estudiantesHref = this.getAttribute('estudiantes-href') || '/views/loginEstudiantes.php';
+    const docentesHref = this.getAttribute('docentes-href') || '/views/loginDocentes.php';
+    const matriculaHref = this.getAttribute('matricula-href') || '/views/login.php';
+    const admisionesHref = this.getAttribute('admisiones-href') || '/views/admisiones.php';
 
         this.innerHTML = `
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
                     <div>
-                    <a href="index.php"><img style="width: 45%;" src="${logo}" alt="${logoAlt}" /></a>
+                    <a href="/index.php"><img style="width: 45%;" src="${logo}" alt="${logoAlt}" /></a>
                     </div>
                     <div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
