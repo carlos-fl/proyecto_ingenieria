b7f18c1042bdbedbfa357382bc0b6c5419eaacd8
carlos <carlosftru@gmail.com>
2025-03-22 11:16:07 -0600

    created user-profile component

---
+++ public/views/js/components/userProfile.js
@@ -0,0 +1,69 @@
+class UserProfile extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+
+  connectedCallback(){
+    this.profileTitle = this.getAttribute('profile-title')
+    this.welcomeMsg = this.getAttribute('welcome-msg')
+    this.profileImg = this.getAttribute('profile-img')
+    this.imgDescription = this.getAttribute('desc-img')
+    this.employeeNumber = this.getAttribute('user-number')
+    this.employeeEmail = this.getAttribute('user-email')
+    this.employeeName = this.getAttribute('user-name')
+    this.employeePhone = this.getAttribute('user-phone')
+    this.render()
+
+  }
+
+  attributeChangedCallback(name, oldValue, newValue){
+    this.connectedCallback()
+  }
+
+  render() {
+    this.innerHTML = `
+                  <div class="container bg-white w-75 p-5">
+                    <div class="profile-container-title">
+                      <div class="profile-bar-h mb-5">
+                        <h3>${ this.profileTitle }</h3>
+                      </div>
+                    </div>
+                    <div class="profile-container-body">
+                      <div class="profile-container-body-message">
+                        <h4 class="profile-container-body-message-text">${ this.welcomeMsg }</h4>
+                      </div>
+                      <div class="d-flex flex-row justify-content-between align-items-center profile-container-body-info">
+                        <div class="profile-container-body-info-img">
+                          <img src=${ this.profileImg } alt=${ this.imgDescription ?? 'Foto perfil del usuario' }>
+                        </div>
+                        <div class="profile-container-body-info-personal">
+                          <section>
+                              <div class="info-card">
+                                <div class="info-header">
+                                  <span>Información Personal</span>
+                                </div>
+                                <hr />
+                                <div class="info-row info-labels">
+                                  <span>Número de Empleado</span>
+                                  <span>Nombre Completo</span>
+                                  <span>Correo</span>
+                                  <span>Teléfono</span>
+                                </div>
+                                <div class="info-row">
+                                  <span id="employeeNumber">${ this.employeeNumber }</span>
+                                  <span id="name">${ this.employeeName }</span>
+                                  <span id="email">${ this.employeeEmail }</span>
+                                  <span id="phone">${ this.employeePhone }</span>
+                                </div>
+                              </div>
+                            </section>
+                        </div>
+                      </div>
+                    </div>
+                  </div> 
+    `
+  }
+}
+
+customElements.define('user-profile', UserProfile)
