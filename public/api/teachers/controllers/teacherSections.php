27abf59cf7ecdeab2557c097061eae2169d0875a
MoralesGuillermo <109401843+MoralesGuillermo@users.noreply.github.com>
2025-03-25 11:23:55 -0600

    Update teacherSections.php revert

--- public/api/teachers/controllers/teacherSections.php
+++ public/api/teachers/controllers/teacherSections.php
@@ -3,7 +3,7 @@
   include_once __DIR__ . '/../../../../utils/classes/Request.php';
   include_once __DIR__ . '/../../../../services/teachers/types/DataResponse.php';
   include_once __DIR__ . '/../../../../services/teachers/services/Teachers.php';
-  include_once __DIR__ . '/../../../../../config/env/Environment.php';
+  include_once __DIR__ . '/../.././../../config/env/Environment.php';
   include_once __DIR__ . '/../../../../utils/classes/Encrypt.php';
 
 
@@ -12,13 +12,13 @@
   Request::isWrongRequestMethod('GET');
   
   if (empty($_SESSION)) {
-    echo json_encode(new DataResponse("failure", error: new ErrorResponse(401, "Unathorized, NSN")));
+    echo json_encode(new DataResponse("failure", error: new ErrorResponse(401, "Unathorized")));
     return;
   }
 
-  $teacherNumber = _GET["teacher-number"] ?? $_SESSION["TEACHER_NUMBER"];
+  $teacherNumber = $_GET['teacher-number'] ?? $_SESSION["TEACHER_NUMBER"];
   if (!$teacherNumber){
-    echo json_encode(new DataResponse("failure", error: new ErrorResponse(401, "Unathorized, NTN")));
+    echo json_encode(new DataResponse("failure", error: new ErrorResponse(401, "Unathorized")));
     return;
   }
   $teacherResponse = TeacherService::getCurrentSections((int) $teacherNumber);
