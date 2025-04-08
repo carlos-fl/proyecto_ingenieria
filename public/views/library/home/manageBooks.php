d7c7f4ef3f359a9e3e6b33a93386870a566ebef0
DaroGard <AlexDG997@gmail.com>
2025-04-08 10:08:26 -0600

    Remove debug message on manageBooks.php

--- public/views/library/home/manageBooks.php
+++ public/views/library/home/manageBooks.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-echo '<pre>';
-print_r($_SESSION);
-echo '</pre>';
+if (empty($_SESSION)) {
+    header('Location: ../../library/login/libraryLogin.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
