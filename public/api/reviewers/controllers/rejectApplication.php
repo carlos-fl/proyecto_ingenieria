4bae5c3097e8273728d0c87336889e308792f759
Carlos Flores <113638566+carlos-fl@users.noreply.github.com>
2025-04-13 18:32:03 -0600

    Update rejectApplication.php

--- public/api/reviewers/controllers/rejectApplication.php
+++ public/api/reviewers/controllers/rejectApplication.php
@@ -68,7 +68,7 @@ try {
         $emailData = ["name" => $userName, "application_code" => $applicationCode, "commentary" => $commentary, "link" => $link];
         $res = ApplicantService::sendResubmissionEmail($userEmail, $token, $emailData);
 
-        echo json_encode(["status" => "success"]);
+        echo json_encode(["status" => "success", "res" => $res]);
     } else {
         throw new Exception("Database error: " . $stmt->error);
     }
